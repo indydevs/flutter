067fbdc75826dcf54ff613bcbb6990009ea90f43
Ali-Akber Saifee <ali@indydevs.org>
2022-09-14 14:33:44 -0700

    Add RSpec integration

---
+++ lib/titli/rspec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "rspec"
+require_relative "tracker"
+require "pry"
+
+module RSpec
+  class << self
+    def tracker
+      @tracker ||= Titli::Tracker.new(["app"])
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:each) do |example|
+    example.metadata[:titli_effect] = false
+    ::RSpec.tracker.start(example.full_description)
+  end
+  config.after(:each) do |_example|
+    ::RSpec.tracker.stop
+  end
+  config.after(:all) do
+    ::RSpec.tracker.persist!
+  end
+end
+
+module Titli
+  module RSpec
+    module ClassMethods
+      def filtered_examples
+        super.select do |example|
+          !::RSpec.tracker.skip?(example.full_description)
+        end
+      end
+    end
+  end
+end
+
+module RSpec
+  module Core
+    class ExampleGroup
+      class << self
+        prepend Titli::RSpec::ClassMethods
+      end
+    end
+  end
+end
