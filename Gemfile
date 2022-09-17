4cfcdb15a4b1b4a6614f6024ae7280a99084152a
Ali-Akber Saifee <ali@indydevs.org>
2022-09-17 14:57:00 -0700

    Add code coverage to tests & CI

--- Gemfile
+++ Gemfile
@@ -14,3 +14,9 @@ gem "rubocop-shopify", require: false
 gem "pry", "~> 0.14.1"
 
 gem "overcommit", "~> 0.59.1", group: :development
+
+gem "simplecov", "~> 0.21.2", groups: [:development, :test]
+
+gem "simplecov-console", "~> 0.9.1", groups: [:development, :test]
+
+gem "simplecov-cobertura", "~> 2.1", groups: [:development, :test]
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,17 @@
 $LOAD_PATH.unshift(File.expand_path("../lib", __dir__))
 require "titli"
 require "minitest"
+require "simplecov"
+
+if ENV["CI"]
+  require "simplecov-cobertura"
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+else
+  require "simplecov-console"
+  SimpleCov.formatter = SimpleCov::Formatter::Console
+end
+
+SimpleCov.start
 
 Minitest.load_plugins
 require "minitest/autorun"
