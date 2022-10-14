05bbf1506e9e57f65a0443cfce4113b499765d8e
Ali-Akber Saifee <ali@indydevs.org>
2022-10-14 15:13:06 -0700

    Add backward compatibility hack for dry-configurable < 0.13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Added
+- Backward compatibilty hack for default values in `dry-configurable < 0.13`
 ## 0.2.5 - 2022-10-13
 ### Added
 - Explicit dependency on `pry` for reflecting sources of methods
--- lib/flutter/config.rb
+++ lib/flutter/config.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "dry/configurable"
+require "dry/configurable/version"
 require_relative "persistence"
 
 module Flutter
@@ -11,12 +12,26 @@ module Flutter
       def included(other)
         other.extend(Dry::Configurable)
         other.class_eval do
-          setting(:enabled, default: false, reader: true)
-          setting(:sources, default: Set.new(["#{Dir.pwd}/*"]))
-          setting(:exclusions, default: Set.new(["./vendor/*"]))
-          setting(:storage_class, default: Flutter::Persistence::Marshal)
-          setting(:storage_options, default: { path: "./.flutter" })
-          setting(:reset_storage, default: false)
+          # dry-configurable versions < 0.13 expect default values
+          # as the second parameter, whereas newer versions have started
+          # deprecating passing the default value as a non keyword argument.
+          if Dry::Configurable::VERSION >= "0.13"
+            setting(:enabled, default: false, reader: true)
+            setting(:sources, default: Set.new(["#{Dir.pwd}/*"]))
+            setting(:exclusions, default: Set.new(["./vendor/*"]))
+            setting(:storage_class, default: Flutter::Persistence::Marshal)
+            setting(:storage_options, default: { path: "./.flutter" })
+            setting(:reset_storage, default: false)
+          else
+            # :nocov:
+            setting(:enabled, false, reader: true)
+            setting(:sources, Set.new(["#{Dir.pwd}/*"]))
+            setting(:exclusions, Set.new(["./vendor/*"]))
+            setting(:storage_class, Flutter::Persistence::Marshal)
+            setting(:storage_options, { path: "./.flutter" })
+            setting(:reset_storage, false)
+            # :nocov:
+          end
         end
       end
     end
